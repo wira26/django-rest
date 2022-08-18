6c692f74722d3e5f26e8eca0c57fb594d7743846
wira sanjaya <sanjayawira02@gmail.com>
2022-08-18 13:43:40 +0700

    wiracode

--- tutorial/settings/base.py
+++ tutorial/settings/base.py
@@ -136,3 +136,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 2
 }
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
